dba8460fed02318dfe3f6d59aa60ac3c1ccc3fa6
ParkHyunsu1110 <phs0767@naver.com>
2025-10-19 00:03:26 +0900

    [박현수] 순서쌍의 개수

---
+++ src/Solution_Ordered_Pair_Count.java
@@ -0,0 +1,14 @@
+public class Solution_Ordered_Pair_Count {
+    public static void main(String[] args) {
+        int result = solution(100);
+        System.out.println(result);
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i =1; i <= n; i++) {
+            if (n % i == 0) answer++;
+        }
+        return answer;
+    }
+}
